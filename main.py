df8f3c848b592f59f06d5b02a5ea203dbd8ad5fd
hiyanshchandel <164910660+hiyanshchandel@users.noreply.github.com>
2025-03-29 20:06:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,5 +56,5 @@ def voicecare_form():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
