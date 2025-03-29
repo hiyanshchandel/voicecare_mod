b4061ec5346c462644ca287337e389ce19523a30
Jatin Agrawal <jatinagrawal0987654321@gmail.com>
2025-03-29 21:25:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def voicecare_form():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Get port from environment
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
