916d499390a44724f834ba9dd4b8c863e48a74fd
hiyanshchandel <164910660+hiyanshchandel@users.noreply.github.com>
2025-04-01 19:26:03 +0530

    Update summary_upsertion.py

--- summary_upsertion.py
+++ summary_upsertion.py
@@ -1,26 +1,26 @@
 from pinecone import Pinecone
 import json
 import uuid
+from embeddings import create_embeddings
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
 pc = Pinecone(api_key = os.environ.get("PINECONE_API_KEY"))
 
-index = pc.Index("voicecaretest3")
+index = pc.Index("voicecare5")
 
 def summary_upsert(username, summary):
     if not summary or summary.strip() == "NO_SUMMARY":
         return
     record_id = str(uuid.uuid4())
-    metadata = [
-        str("user_id :" + str(username)),
-        str("category:" + "summary")
-    ]
+    metadata = {
+        "user_id" : username,
+        "content" : summary,
+        "type" : "summary"
+    }
     
-    records = [{"_id": record_id, "chunk_text": summary, "metadata": metadata}]
-    index.upsert_records(namespace = username, records = records)
-    print(record_id)
+    records = [{"id": record_id, "values": create_embeddings(json.dumps(summary, indent=2)), "metadata": metadata}]
+    index.upsert(namespace = username, vectors=records)
     print(f"Summary for User {username} inserted into Pinecone.")
 
 
